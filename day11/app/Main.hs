d98dda2b78bd2bce468024a0b86ac1428f037d9a
joverandout <u1917702@live.warwick.ac.uk>
2020-12-15 02:20:59 +0000

    working neighbour function

--- day11/app/Main.hs
+++ day11/app/Main.hs
@@ -4,9 +4,10 @@ import Control.Arrow ((&&&))
 import qualified Data.Map as M
 import Data.Map (Map)
 
-type Plan = Map (Int, Int) Pos
+type Plan = Map Doubly Pos
 type Input = Plan
-data Pos = Floor | Empty | Taken deriving Show
+data Pos = Floor | Empty | Taken deriving (Eq, Show)
+type Doubly = (Int, Int)
 
 prepare :: String -> Input
 prepare input = M.fromList $ do
@@ -18,10 +19,27 @@ prepare input = M.fromList $ do
         parse 'L' = Empty
         parse '#' = Taken
 
-partA = id
+iterate :: Plan -> Doubly -> Pos -> Pos
+iterate state doub p = case p of
+    Floor -> Floor
+    Empty | (length . filter (==Taken) . map (flip (M.findWithDefault Floor) state) . neighbouring $ doub) == 0 -> Taken
+          | otherwise -> Empty
+    Taken | (length . filter (==Taken) . map (flip (M.findWithDefault Floor) state) . neighbouring $ doub) >= 4 -> Empty
+          | otherwise -> Taken
+
+--length . filter (==Taken) . map (flip (M.findWithDefault Floor) state) . neighbors $ doub
+
+first = id
 
 get :: IO String
 get = readFile "seatingPlan.txt"
 
 main :: IO ()
-main = get >>= print . (partA) . prepare
+main = get >>= print . (first) . prepare
+
+neighbouring :: Doubly -> [Doubly]
+neighbouring x = map (add x) . tail $ do
+    dy <- [0,1,-1]
+    dx <- [0,1,-1]
+    pure (dy, dx)
+    where add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
