d74ee050c36d7a66a7e9d6ec0c7f0c2050e4dfec
joverandout <u1917702@live.warwick.ac.uk>
2020-12-16 16:22:03 +0000

    part 2

--- day12/app/Main.hs
+++ day12/app/Main.hs
@@ -9,11 +9,11 @@ type Pos = (Int, Int)
 data Compass = N | E | S | W deriving Show
 data Relative = F | L | R deriving Show
 data Ship = Ship {position :: Pos, heading :: Compass}
-data Instruction = Instruction Heading Int deriving Show
+data Command = Command Heading Int deriving Show
 data Heading = Absolute Compass | Relative Relative deriving Show
 data Traveler = Traveler {boat, marker :: Pos} deriving Show
 
-type Input = [Instruction]
+type Input = [Command]
 
 get :: IO String
 get = readFile "input.txt"
@@ -34,7 +34,7 @@ move n d (x, y) = let (dx, dy) = toDelta d
 
 manVal :: String -> Input
 manVal = map parse . lines
-  where parse (c:n) = Instruction d (read n)
+  where parse (c:n) = Command d (read n)
           where d = case c of
                       'N' -> Absolute N
                       'S' -> Absolute S
@@ -44,8 +44,8 @@ manVal = map parse . lines
                       'R' -> Relative R
                       'L' -> Relative L
 
-follow :: Ship -> Instruction -> Ship
-follow (Ship pos h) (Instruction t n) = case t of
+follow :: Ship -> Command -> Ship
+follow (Ship pos h) (Command t n) = case t of
   Absolute c -> Ship (move n c pos) h
   Relative F -> Ship (move n h pos) h
   Relative r -> Ship pos h'
@@ -61,18 +61,18 @@ toDelta x = case x of
     W -> (-1, 0)
     E -> (1, 0)
 
-markerpart2 :: Traveler -> Instruction -> Traveler
-markerpart2 (Traveler pos wp) (Instruction t n) = case t of
+markerpart2 :: Traveler -> Command -> Traveler
+markerpart2 (Traveler pos wp) (Command t n) = case t of
   Absolute c -> Traveler pos (move n c wp)
-  Relative F -> Traveler pos' wp
-    where pos' = let (x, y) = pos
-                     (dx, dy) = wp
-                 in (x + n * dx, y + n * dy)
+  Relative F -> Traveler (posCalc n pos wp) wp
   Relative R -> Traveler pos (right wp n 1)
   Relative L -> Traveler pos (right wp n 3)
 
 right :: (Int, Int) -> Int -> Int -> (Int, Int)
 right wp n m = iterate turnRight wp !! (n * m)
+
+posCalc :: Int -> (Int, Int) -> (Int, Int) -> (Int, Int)
+posCalc n (x,y) (dx, dy) = (x + n * dx, y + n * dy)
         
 turnRight :: (Int, Int) -> (Int, Int)
 turnRight (x, y) = (y, -x)
@@ -80,8 +80,8 @@ turnRight (x, y) = (y, -x)
 main :: IO ()
 main = do
        putStrLn "++++++++++++++++"
-       putStr "part 1: "
+       putStr   "part 1: "
        get >>= print . (part1 . position . foldl' follow (Ship (0,0) E)) . manVal
-       putStr "part: 2 "
+       putStr   "part: 2 "
        get >>= print . (part1 . boat .foldl' markerpart2 (Traveler (0, 0) (10, 1))) . manVal
        putStrLn "++++++++++++++++"
