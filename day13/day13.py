31dff8f16d07feb37255c28d527259b6d5588efd
joverandout <u1917702@live.warwick.ac.uk>
2020-12-16 18:37:22 +0000

    part 2 working with the chinese remainder theroum

--- day13/day13.py
+++ day13/day13.py
@@ -1,5 +1,24 @@
 data = [line.strip() for line in open("input.txt", 'r')]
 
+def gcd(a,b):
+    if a == 0:
+        return (b, 0, 1)
+    g, y, x = gcd(b % a, a)
+    return (g, x - (b // a) * y ,y)
+
+def modu(n, p):
+    g, inv, y = gcd(n, p)
+    assert g == 1
+    return inv % p
+
+def chinese_remainder_theorem(buses, modulo):
+    x = 0
+    for a, p in buses:
+        n = modulo // p
+        inverse = modu(n, p)
+        x = (x+a*n*inverse) % modulo
+    return x % modulo
+
 departure_time = data[0]
 values = data[1].split(',')
 usedVals = []
@@ -17,4 +36,17 @@ for val in usedVals:
 
 bus = finalValues.index(min(finalValues))
 
-print((finalValues[bus] - int(departure_time)) * int(usedVals[bus]))
+print("Part A: "+ str((finalValues[bus] - int(departure_time)) * int(usedVals[bus])))
+
+def part2(buses):
+    modulo = 1
+    for i in buses:
+        modulo *= i[1]
+    return(chinese_remainder_theorem(buses, modulo))
+
+part_2 = []
+for counter, i in enumerate(data[1].split(',')):
+    if i != "x":
+        part_2.append((-counter, int(i)))
+        
+print("Part B: " + str(part2(part_2)))
