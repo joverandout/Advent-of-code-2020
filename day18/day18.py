8eeaca3725af8c7e7aa3caf03f7add02441e7ef2
joverandout <u1917702@live.warwick.ac.uk>
2020-12-19 13:43:00 +0000

    part 1 done

---
+++ day18/day18.py
@@ -0,0 +1,81 @@
+import re
+import string
+
+def concat(first, last, sum):
+    total = ""
+    for i in range(first, last+1):
+        total = total + sum[i]
+    return total
+    
+def countbracs(line):
+    for char in line:
+        if char == "(" or char == ")":
+            return True
+    return False
+
+def do_sum(sum):
+    expr = []
+    first = 0
+    for i in range(0, len(sum)):
+        if sum[i] == " ":
+            expr.append(str(concat(first, i-1, sum)))
+            first = i+1
+    expr.append(str(concat(first, len(sum)-1, sum)))
+    value = int(expr[0])
+    for i in range(1, len(expr), 2):
+        op = expr[i]
+        if op == "+":
+            value = value + int(expr[i+1])
+        elif op == "*":
+            value = value * int(expr[i+1])
+        else:
+            raise ValueError(f"bad op: {op}")
+    return value
+
+def get_number_of_brackets(line):
+    bracs = []
+    for char in line:
+        if char == "(" or char == ")":
+            bracs.append(char)
+    return bracs
+
+def eval_line(line, bracket_order):
+    for i in range(len(bracket_order)):
+        if bracket_order[i] == "(" and bracket_order[i+1] == ")":
+            return(eval_bracket(line, i))
+
+def eval_bracket(line, bracket_index):
+    index1 = 0
+    index2 = 0
+    firstBrac = False
+    bracket_index2 = -1
+    for count in range(len(line)):
+        if line[count] == "(":
+            bracket_index2 += 1
+            if(bracket_index2 == bracket_index):
+                index1 = count
+                firstBrac = True
+        if(firstBrac == True and line[count] == ")"):
+            index2 = count-1
+            break
+    swap_val = do_sum(line[index1+1:index2+1])
+    new_line = ""
+    done = False
+    for i in range(len(line)):
+        if i<index1 or i > index2+1:
+            new_line += line[i]
+        elif(done == False):
+            done = True
+            new_line += str(swap_val)
+    return new_line
+
+total = 0
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+for line in lines:
+    line2 = line
+    while(countbracs(line2) == True):
+        line2 = eval_line(line2, get_number_of_brackets(line2))
+    line2 = do_sum(line2)
+    total = total + line2
+print(total)
